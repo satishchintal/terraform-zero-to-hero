7a4fab795c00cf1640db53e68d9ffa9c599d562d
satishchintal <97095294+satishchintal@users.noreply.github.com>
2026-01-11 18:20:58 +0000

    new commit

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -5,4 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-hdjfj"
+    key_name = "aws-key"
 }
